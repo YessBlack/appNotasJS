cb959693e6ece1b9f54f3d3adee5405de2c9b88b
Angelica Garcia <anyegar36@gmail.com>
2022-08-26 19:35:17 -0500

    Creacion del componente add task

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 import $header from './components/Header.js'
+import $form from './components/FormAddTask.js'
 
 const $app = document.getElementById('app')
 
 window.addEventListener('DOMContentLoaded', e => {
     $app.appendChild($header)
+    $app.appendChild($form)
+
+    const btnAdd = document.getElementById('btn-add-task')
+    
+    btnAdd.addEventListener('click', e => {
+        e.preventDefault()
+        console.log(e)
+    })
 })
---
+++ js/components/FormAddTask.js
@@ -0,0 +1,9 @@
+const $form = document.createElement('form')
+$form.classList.add('form')
+$form.innerHTML = `
+                        <input type="text" placeholder="Añadir Tarea" class="input-task">
+                        <button type="submit" class="btn-add-task" id='btn-add-task'><ion-icon name="add-circle-outline"></ion-icon></button>
+                    `
+
+export default $form
+
